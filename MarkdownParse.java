db793fe6cd6aa947037613e8b0a9816b0e869bdc
NickAzp <69650461+NickAzp@users.noreply.github.com>
2022-04-15 11:12:49 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -9,18 +9,35 @@ public class MarkdownParse {
 
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
+        
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
+        int PreviousIndex = 0;
+        int iteration = 1;
+        while(currentIndex < markdown.indexOf(")",markdown.length()-2)) {
+            PreviousIndex = markdown.indexOf("](", currentIndex);
+            if(markdown.indexOf(")", currentIndex) - markdown.indexOf("[", currentIndex) == -3) {
+                currentIndex = markdown.indexOf("[", PreviousIndex);
+                toReturn.add(markdown.substring(PreviousIndex, currentIndex));
+                System.out.println(currentIndex);
+            } else {
+                currentIndex = markdown.indexOf(")")+iteration;
+                iteration++;
+            }
+            
+            /*
+            currentIndex = markdown.indexOf(")", openParen);
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
+            */
         }
-
-        return toReturn;
+        // toReturn.add(markdown.substring(markdown.indexOf("]("), markdown.length()));
+       //  return toReturn; // markdown.substring(markdown.indexOf("]("), markdown.length));
+       return toReturn;
     }
 
 
